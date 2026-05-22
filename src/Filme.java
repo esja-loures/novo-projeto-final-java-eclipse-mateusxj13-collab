0e8ec6034f2d52aec8ef4e4b0962b3c4c6a94936
mateusxj13 <mateusxj13@gmail.com>
2026-05-22 09:46:24 +0100

    Vlw José🫡😅

--- src/Filme.java
+++ src/Filme.java
@@ -3,20 +3,25 @@
  */
 public class Filme {
 	private String titulo;
+	private int ano;
 	// Falta um atributo para ano
 	
-	public Filme(String titulo) { // o construtor tem de aceitar o atributo ano
+	public Filme(String titulo, int ano) { // o construtor tem de aceitar o atributo ano
 		this.titulo = titulo;
+		this.ano = ano;
 	}
 	// Completa aqui o método Setter para alterar o título e o ano do filme
-	public void setFilme(String titulo) {
+	public void setFilme(String titulo, int ano) {
 		this.titulo = titulo;
+		this.ano = ano;
 	}
 	// Completa aqui o método toString para apresentar o título e o ano do filme
 	public String toString() {
 		
 		StringBuilder sb = new StringBuilder("");
-		sb.append("Título: "+titulo);
+		sb.append("Título: "+ titulo);
+		sb.append("Título: "+ ano);
+		
 		return sb.toString();
 	}
 }
